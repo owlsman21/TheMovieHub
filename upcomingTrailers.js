3a500e98bdc1b716a386be07d9f22b882e8807be
Dennis Haywood <owlsman1@gmail.com>
2025-06-03 11:40:20 +0700

    added trailers

--- upcomingTrailers.js
+++ upcomingTrailers.js
@@ -2,32 +2,61 @@
 
 const upcomingTrailers = [
     {
-        title: "Deadpool & Wolverine Teaser",
-        description: "They're entering the MCU. Expected Summer 2024.",
-        embedId: "Qp4b-5P-QxI", // Actual YouTube ID for the teaser
+        title: "Greyhound (2020) Movie | Tom Hanks, Stephen Graham, Stephen Graham ",
+        description: "Several months after the U.S. entry into World War II, an inexperienced U.S. Navy commander must lead an Allied convoy being stalked by a German submarine wolf pack.",
+        embedId: "DggVVUDUg_E", // Actual YouTube ID for the teaser
         source: "youtube",
         releaseDate: "2024-07-26"
     },
     {
-        title: "Inside Out 2 Official Trailer",
-        description: "New emotions join Riley's mind. Expected June 2024.",
-        embedId: "LEqQeF-34iM", // Actual YouTube ID for the trailer
+        title: "Superman | Official Trailer | DC",
+        description: "Superman must reconcile his alien Kryptonian heritage with his human upbringing as reporter Clark Kent. As the embodiment of truth, justice and the human way he soon finds himself in a world that views these as old-fashioned.",
+        embedId: "Ox8ZLF6cGM0", // Actual YouTube ID for the trailer
         source: "youtube",
         releaseDate: "2024-06-14"
     },
     {
-        title: "Mufasa: The Lion King - First Look",
-        description: "A prequel to The Lion King. Expected December 2024.",
-        embedId: "mX4oJ9QY29c", // Actual YouTube ID
+        title: "Frankenstein | Guillermo del Toro | Official Teaser ",
+        description: "A brilliant but egotistical scientist brings a creature to life in a monstrous experiment that ultimately leads to the undoing of both the creator and his tragic creation",
+        embedId: "x--N03NO130", // Actual YouTube ID
         source: "youtube",
         releaseDate: "2024-12-20"
     },
     {
-        title: "Twisters - Official Trailer",
-        description: "A new take on the classic disaster film. Expected July 2024.",
-        embedId: "j-g4q4M_yA4", // Actual YouTube ID
+        title: "IT: Welcome to Derry | Official Teaser",
+        description: "It will follow the events in the 1960s, the time leading up to the events of the first film in the Stephen King It series.",
+        embedId: "6HG-wsRsg8s", // Actual YouTube ID
         source: "youtube",
         releaseDate: "2024-07-19"
     },
+    {
+        title: "Black Phone 2 | Official Trailer",
+        description: "The phone is ringing again. Following the blockbuster success of Blumhouse’s 2022 horror phenomenon, which earned more than $160 million worldwide and global critical acclaim, Universal Pictures announces the launch of a sinister new franchise with the release of Black Phone 2.",
+        embedId: "v0kqkRZHqk4", // Actual YouTube ID
+        source: "youtube",
+        releaseDate: "2024-07-19"
+    },
+    {
+        title: "",
+        description: "",
+        embedId: "", // Actual YouTube ID
+        source: "youtube",
+        releaseDate: "2024-07-19"
+    },
+    {
+        title: "",
+        description: "",
+        embedId: "", // Actual YouTube ID
+        source: "youtube",
+        releaseDate: "2024-07-19"
+    },
+    {
+        title: "",
+        description: "",
+        embedId: "", // Actual YouTube ID
+        source: "youtube",
+        releaseDate: "2024-07-19"
+    },
+
     // Add more upcoming movie trailers here!
 ];
